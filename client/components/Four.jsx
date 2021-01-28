9200ae2b606fa7aa754f3853f91b81108d3b6747
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-29 12:06:40 +1300

    add random buttons on page 4

--- client/components/Four.jsx
+++ client/components/Four.jsx
@@ -6,6 +6,8 @@ function Four () {
   return (
     <div className="container">
     <img src="/images/gnome4.png" alt="windowsXP with a Gnome"/>
+    <Link className="btn1" to= '/game/eight'>4</Link>
+    <Link className="btn2" to= '/game/five'>4</Link>
     <Link className="btn3" to= '/game/three'>4</Link>
     </div>
   )
