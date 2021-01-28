41ec3a9d58e17f6f8fb4f7836ffb660c72efca7c
Isaac-Sedcole <isaacsedcolei@gmail.com>
2021-01-28 21:15:02 +1300

    added sound module and folder

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
+import Sound from 'react-sound'
 
 const App = () => {
   return (
     <>
     <h1>React development has begun!</h1>
     <Route/>
+    <Sound/>
     </>
   )
 }
