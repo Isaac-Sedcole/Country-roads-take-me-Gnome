55adf236556fe734a793b307e4cd0fc6f08d0729
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-29 11:28:18 +1300

    changed file structure slightly and attempted sound

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     <Route path='/' exact component={Home}/>
 
 
-    <Sound/>
+    {/* <Sound/> */}
       </>
   )
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+// import Render from './Render'
 // import {Route} from 'react-router-dom'
-import Sound from 'react-sound'
-import Eight from './Eight'
+// import Sound from 'react-sound'
+// import Sound from 'react-sound'
+// import Sound from './Sound'
+
+// function render(){
+//   return(
+// <>
+{/* < Sound  */}
+  // url='/sounds/birdsB.mp3'
+  // autoLoad={true}
+  // playStatus={Sound.status.PLAYING}
+  // playFromPosition={300 /* in milliseconds */}
+  // onLoading={this.handleSongLoading}
+  // onLoading={this.handleSongLoading}
+  // onPlaying={this.handleSongPlaying}
+  // onFinishedPlaying={this.handleSongFinishedPlaying}
+  // />
+  // </>
+  // );
+// }
 
 function Home (){
   return (
+    <div>
+      {/* <button onClick={render}></button> */}
+  {/* // <div onLoad={render}> */}
+    {/* < Sound 
+  url='/sounds/birdsB.mp3'
+  autoLoad={true}
+  playStatus={Sound.status.PLAYING} */}
+  {/* // playFromPosition={300 /* in milliseconds */}
+  {/* // onLoading={this.handleSongLoading} */}
 
-  <>
-  < Sound url="../sounds/birds.wav" 
-    playStatus={Sound.status.PLAYING} /> 
+  {/* // /> */}
+  {/* <Render/> */}
+  {/* <Sound url='/sounds/birdsB.mp3'playStatus={Sound.status.PLAYING}/> */}
   <button><Link to={`/game/eight`}> TEST </Link></button>
-  </>
-  )
+  </div>
+  );
 }
 
 
