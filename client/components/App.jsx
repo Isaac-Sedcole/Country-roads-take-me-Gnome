bc842dab62f57cfd478b1abb37f89f45d8b3fb71
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-29 10:11:01 +1300

    add components to app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,35 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import Sound from 'react-sound'
 
+import Eight from './Eight'
+import Seven from './Seven'
+import Six from './Six'
+import Five from './Five'
+import Four from './Four'
+import Three from './Three'
+import Two from './Two'
+import One from './One'
+import Zero from './Zero'
+
+import { HashRouter as Router, Route } from 'react-router-dom'
+
 const App = () => {
   return (
     <>
+    <Router>
     <h1>React development has begun!</h1>
-    <Route/>
+    <Route path='/game/eight' exact component={Eight}/>
+    <Route path='/game/seven' exact component={Seven}/>
+    <Route path='/game/six' exact component={Six}/>
+    <Route path='/game/five' exact component={Five}/>
+    <Route path='/game/four' exact component={Four}/>
+    <Route path='/game/three' exact component={Three}/>
+    <Route path='/game/two' exact component={Two}/>
+    <Route path='/game/one' exact component={One}/>
+    <Route path='/game/zero' exact component={Zero}/>
+    
     <Sound/>
+    </Router>
     </>
   )
 }
--- client/components/Zero.jsx
+++ client/components/Zero.jsx
@@ -1,2 +1,11 @@
+import React from 'react'
+
+function Zero () {
+
+  return (
+    <h1>ARRRRRRRRGGGGGGGGGHHHHHHHHH</h1>
+  )
+  
+}
 
 export default Zero
