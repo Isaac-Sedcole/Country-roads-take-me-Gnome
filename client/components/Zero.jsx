18df8a2e6992f192744b88b5324ea30572bf990c
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-29 12:09:57 +1300

    struggling to get image interval working

--- client/components/Zero.jsx
+++ client/components/Zero.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-setTimeout(() => {
-  //stuff
-},5000)
 
 function Zero () {
+  
+  useEffect(() => {
+    const work = setTimeout(() => {
+      setGnome({
+        img: (<img src="/images/gnomeSolo.png"/>)
+      })
+    },3000)
+    return () => clearInterval(work)
+  })
+
+  const [gnome, setGnome] = useState({
+    img: (<img src="/images/scary-image1.png"/>),
+  })
+
 
   return (
+    <>
     <h1>ARRRRRRRRGGGGGGGGGHHHHHHHHH</h1>
+
+    <div>
+      {console.log(gnome)}
+      {gnome}
+    </div>
+    </>
   )
   
 }
