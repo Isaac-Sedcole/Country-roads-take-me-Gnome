da0a3f48ccbe6edffc62625febedb1c98b043034
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-29 14:03:49 +1300

    added a welcome - welcome page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,6 +8,7 @@ import Sound from 'react-sound'
 
 function Home (){
   const [playing, setPlaying] = useState(false)
+  const [ isStarted, setIsStarted ] = useState(false)
   return (
     <>
     {/* <div className="container" onMouseMove={() => {
@@ -17,15 +18,20 @@ function Home (){
      <ReactAudioPlayer src="/sounds/birdsB.mp3"
      autoPlay={playing ? true : false} />
     </div> */}
-    {/* <div className="container" onMouseMove={() => {setPlaying(true)}}>
-    <Sound url='/sounds/birdsB.mp3'autoLoad={true} playStatus={playing ? Sound.status.PLAYING: Sound.status.STOPPED} />
-      <img src="/images/tree.png"/>
-</div> */}
-    <h3>click on image :)</h3>
-      <button onClick={() => {setPlaying(true)}}> <img src="/images/tree.png"/></button>
-    <button><Link to={`/game/eight`}> Country roads, take me Gnome </Link></button>
-      <Sound url='/sounds/birdsB.mp3'autoLoad={true} playStatus={playing ? Sound.status.PLAYING: Sound.status.STOPPED} />
+    { !isStarted && (
+      <button onClick={() => setIsStarted(true)}>Get started!</button>
+    )}
+    { isStarted && (
+      <div className="container" onMouseMove={() => {setPlaying(true)}}>
+        <Sound url='/sounds/birdsB.mp3'autoLoad={true} playStatus={playing ? Sound.status.PLAYING: Sound.status.STOPPED} />
+        <img src="/images/tree.png"/>
+      </div>
+    )}
   
+  {/* <h3>click on image :)</h3>
+    <button onClick={() => {setPlaying(true)}}> <img src="/images/tree.png"/></button>
+  <button><Link to={`/game/eight`}> Country roads, take me Gnome </Link></button>
+    <Sound url='/sounds/birdsB.mp3'autoLoad={true} playStatus={playing ? Sound.status.PLAYING: Sound.status.STOPPED} /> */}
   </>
   );
 }
