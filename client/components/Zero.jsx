966aff6be827c3f0dd202ab83dd2a7d0cee1adab
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-29 14:52:06 +1300

    add sound to zero page

--- client/components/Zero.jsx
+++ client/components/Zero.jsx
@@ -1,15 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-
-function Zero () {
-  
+function Zero() {
   return (
+    <>
+      <div className="container">
+        <img
+          id="scary-image"
+          src="/images/scary-image1.png"
+          alt="aaaaaaarrrrrrrrrrrggggggggghhhhhhhhhh"
+        />
+      </div>
 
-    <div className="container">
-    <img id="scary-image" src="/images/scary-image1.png" alt="aaaaaaarrrrrrrrrrrggggggggghhhhhhhhhh"/>
-     </div>
-  )
-  
+      <div
+        className="container"
+        onMouseMove={() => {
+          setPlaying(true);
+        }}
+      >
+        <Sound
+          url="/sounds/birdsB.mp3"
+          autoLoad={true}
+          playStatus={playing ? Sound.status.PLAYING : Sound.status.STOPPED}
+        />
+      </div>
+    </>
+  );
 }
 
-export default Zero
+export default Zero;
